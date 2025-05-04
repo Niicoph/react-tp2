85baec351096a4847c2d3c458f49500683882330
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2025-05-04 20:44:22 -0300

    fix: bandera no se actualizaba segun estado de local

--- src/Components/Header/LanguageSelector/LanguageSelector.jsx
+++ src/Components/Header/LanguageSelector/LanguageSelector.jsx
@@ -1,33 +1,38 @@
-import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next'
 
 const languages = [
     { code: 'en', name: 'English', flag: '/src/assets/Icons/en-UK.png' },
     { code: 'es', name: 'Español', flag: '/src/assets/Icons/es-ES.png'},
   ];
 
-export default function LanguageSelector() { 
-    
-    const { i18n, t } = useTranslation(); // ← usamos t para que el componente reaccione a cambios
-
-    const currentLang = languages.find((lang) => lang.code === i18n.language) || languages[0];
+  export default function LanguageSelector() {
+    const { i18n } = useTranslation();
+    const [currentLangCode, setCurrentLangCode] = useState(localStorage.getItem('language') || i18n.language);
+  
+    useEffect(() => {
+      i18n.changeLanguage(currentLangCode);
+      localStorage.setItem('language', currentLangCode);
+    }, [currentLangCode, i18n]);
+  
+    const currentLang = languages.find((lang) => lang.code === currentLangCode) || languages[0];
   
     const handleChange = (e) => {
-      const selectedLang = e.target.value;
-      i18n.changeLanguage(selectedLang);
-      localStorage.setItem('language', selectedLang);
+      setCurrentLangCode(e.target.value);
     };
-
-
-    return(
-        <div className="w-full h-16 flex items-center border-b border-dotted border-slate-300 sticky top-0 z-10 bg-opacity-90 backdrop-blur-sm px-2">
-            <img className="w-6 h-6 rounded-full mr-2" src={currentLang.flag} alt={currentLang.code} />
-            <select value={t.language} onChange={handleChange}>
-            {languages.map((lang) => (
-                <option key={lang.code} value={lang.code}>
-                    {lang.name}
-                </option>
-            ))}
-            </select>
-        </div>
+  
+    return (
+      <div className="w-full h-16 flex items-center border-b border-dotted border-slate-300 sticky top-0 z-10 bg-opacity-90 backdrop-blur-sm px-2">
+  
+        <img className="w-6 h-6 rounded-full m-2" src={currentLang.flag} alt={currentLang.code} />
+  
+        <select value={currentLangCode} onChange={handleChange}>
+          {languages.map((lang) => (
+            <option key={lang.code} value={lang.code}>
+              {lang.name}
+            </option>
+          ))}
+        </select>
+      </div>
     );
-}
+  }
