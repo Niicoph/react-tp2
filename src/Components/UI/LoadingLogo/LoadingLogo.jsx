d3dfd785ee39902b890b4292684cd6ea0be0ad72
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2025-05-06 21:17:26 -0300

    feat: sombra al logo loading

--- src/Components/UI/LoadingLogo/LoadingLogo.jsx
+++ src/Components/UI/LoadingLogo/LoadingLogo.jsx
@@ -4,7 +4,7 @@ import Styles from './LoadingLogo.module.css';
 export default function LoadingLogo() {
   return (
     <div className="absolute h-screen w-screen flex justify-center items-center z-50 bg-image">
-      <img src={Loading} alt="Loading..." className={`${Styles.rotar_y}`}></img>
+      <img src={Loading} alt="Loading..." className={`logo-shadow  ${Styles.rotar_y}`}></img>
     </div>
   )
 }
